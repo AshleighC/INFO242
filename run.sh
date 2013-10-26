6b6d6bc4fd9bfb4c92b4059048a3d4bebfa9ed3a
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-10-26 02:48:12 -0700

    Made run.sh cooler.

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 DATA=2013-10-23-14.json
 FILE=data.json
+COUNT=5
 
-wget http://data.githubarchive.org/$DATA.gz
-gunzip $DATA.gz
-head -$1 $DATA > $FILE
+if [ ! -f $DATA ]
+then
+  wget http://data.githubarchive.org/$DATA.gz
+  gunzip $DATA.gz
+fi
+
+if [ $# -eq 1 ]
+then
+  COUNT=$1
+fi
+
+head -$COUNT $DATA > $FILE
 python converter.py $FILE > data.xml
 
