6d748cdec4d05fe40aab1d3ef700aa8cf2a4d6fb
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2013-11-06 15:32:19 -0800

    Cleaned up transformer.py but still ugly.

--- transformer.py
+++ transformer.py
@@ -3,16 +3,16 @@ from bs4 import BeautifulSoup
 from converter import pretty_print, print_simple
 
 def print_event_actor(actor, indent, spaces):
-  pretty_print('<actor type="%s">' % actor.type.string, indent)
-  print_simple('login', actor.login.string, indent + spaces)
+  pretty_print('<actor type="%s">' % actor.find('type').string, indent)
+  print_simple('login', actor.find('login').string, indent + spaces)
   if actor.find('name'):
     print_simple('name', actor.find('name').string, indent + spaces)
-  if actor.email:
-    print_simple('email', actor.email.string, indent + spaces)
-  if actor.location:
-    print_simple('location', actor.location.string, indent + spaces)
-  if actor.blog:
-    print_simple('blog', actor.blog.string, indent + spaces)
+  if actor.find('email'):
+    print_simple('email', actor.find('email').string, indent + spaces)
+  if actor.find('location'):
+    print_simple('location', actor.find('location').string, indent + spaces)
+  if actor.find('blog'):
+    print_simple('blog', actor.find('blog').string, indent + spaces)
   pretty_print('</actor>', indent)
 
 def print_event(event, indent, spaces):
@@ -22,8 +22,10 @@ def print_event(event, indent, spaces):
 def print_events(events, indent, spaces):
   pretty_print('<events>', indent)
   for event in events:
-    if event.repository:
-      pretty_print('<event type="%s" repoID="%s">' % (event.find('type', recursive=False).string, event.repository.id.string), indent + spaces)
+    if event.find('repository'):
+      pretty_print('<event type="%s" repoID="%s">'
+          % (event.find('type', recursive=False).string,
+          event.find('repository').find('id').string), indent + spaces)
       print_event(event, indent + (2 * spaces), spaces)
       pretty_print('</event>', indent + spaces)
   pretty_print('</events>', indent)
@@ -36,35 +38,43 @@ def print_repo_basics(repo, indent, spaces):
 
 def print_repo_dates(repo, indent, spaces):
   pretty_print('<dates>', indent)
-  print_simple('date', repo.created_at.string, indent + spaces, {'of': 'creation'})
-  print_simple('date', repo.pushed_at.string, indent + spaces, {'of': 'push'})
+  print_simple('date', repo.find('created_at').string,
+      indent + spaces, {'of': 'creation'})
+  print_simple('date', repo.find('pushed_at').string,
+      indent + spaces, {'of': 'push'})
   pretty_print('</dates>', indent)
 
 def print_repo_urls(repo, indent, spaces):
   pretty_print('<urls>', indent)
-  print_simple('url', repo.url.string, indent + spaces, {'of': 'github'})
-  if repo.homepage and repo.homepage.string:
-    print_simple('url', repo.homepage.string, indent + spaces, {'of': 'homepage'})
-  if repo.has_wiki.string != 'False':
-    print_simple('url', repo.url.string + '/wiki', indent + spaces, {'of': 'wiki'})
+  print_simple('url', repo.find('url').string,
+      indent + spaces, {'of': 'github'})
+  if repo.find('homepage') and repo.find('homepage').string:
+    print_simple('url', repo.find('homepage').string,
+        indent + spaces, {'of': 'homepage'})
+  if repo.find('has_wiki').string != 'False':
+    print_simple('url', repo.find('url').string + '/wiki',
+        indent + spaces, {'of': 'wiki'})
   pretty_print('</urls>', indent)
 
 def print_repo_languages(repo, indent, spaces):
   pretty_print('<languages>', indent)
-  if repo.language:
-    print_simple('language', repo.language.string, indent + spaces)
+  if repo.find('language'):
+    print_simple('language', repo.find('language').string, indent + spaces)
   pretty_print('</languages>', indent)
 
 def print_repo_metrics(repo, indent, spaces):
   pretty_print('<metrics>', indent)
   pretty_print('<metric type="size">', indent + spaces)
-  print_simple('value', repo.size.string, indent + (2 * spaces))
+  print_simple('value', repo.find('size').string, indent + (2 * spaces))
   print_simple('unit', 'bytes', indent + (2 * spaces))
   pretty_print('</metric>', indent + spaces)
   pretty_print('<metric type="network">', indent + spaces)
-  print_simple('relation', repo.watchers.string, indent + (2 * spaces), {'description': 'watchers'})
-  print_simple('relation', repo.forks.string, indent + (2 * spaces), {'description': 'forks'})
-  print_simple('relation', repo.open_issues.string, indent + (2 * spaces), {'description': 'issues'})
+  print_simple('relation', repo.find('watchers').string,
+      indent + (2 * spaces), {'description': 'watchers'})
+  print_simple('relation', repo.find('forks').string,
+      indent + (2 * spaces), {'description': 'forks'})
+  print_simple('relation', repo.find('open_issues').string,
+      indent + (2 * spaces), {'description': 'issues'})
   pretty_print('</metric>', indent + spaces)
   pretty_print('</metrics>', indent)
 
@@ -79,10 +89,11 @@ def print_repos(repos, indent, spaces):
   repo_ids = []
   pretty_print('<repositories>', indent)
   for repo in repos:
-    repo_id = repo.id.string
+    repo_id = repo.find('id').string
     if repo_id not in repo_ids:
       repo_ids.append(repo_id)
-      pretty_print('<repository id="%s" fork="%s">' % (repo_id, repo.fork.string.lower()), indent + spaces)
+      pretty_print('<repository id="%s" fork="%s">'
+          % (repo_id, repo.find('fork').string.lower()), indent + spaces)
       print_repo(repo, indent + (2 * spaces), spaces)
       pretty_print('</repository>', indent + spaces)
   pretty_print('</repositories>', indent)
